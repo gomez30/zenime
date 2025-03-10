6b43d154f4b0cc8af02d0e96e4b4d3965333d345
itzzzme <sayandas.devs@gmail.com>
2025-03-10 11:30:14 +0530

    Update ContinueWatching.jsx

--- src/components/continue/ContinueWatching.jsx
+++ src/components/continue/ContinueWatching.jsx
@@ -57,11 +57,9 @@ const ContinueWatching = () => {
         <Swiper
           ref={swiperRef}
           className="w-full h-full"
-          slidesPerView={2}
+          slidesPerView={3}
           spaceBetween={15}
           breakpoints={{
-            320: { slidesPerView: 2, spaceBetween: 10 },
-            480: { slidesPerView: 3, spaceBetween: 12 },
             640: { slidesPerView: 4, spaceBetween: 15 },
             768: { slidesPerView: 4, spaceBetween: 15 },
             1024: { slidesPerView: 5, spaceBetween: 15 },
